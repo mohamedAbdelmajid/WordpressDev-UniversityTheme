4cce9fbdaa351bf42bc4a42f79ab26b3c4317fe8
Mohamed Abdelmajid Boutahar <mohamedabdelmajidb@gmail.com>
2024-04-03 15:44:15 +0000

    add two files: single-event.php & archive-event.php, Add Custom Post Types, Order Custom Query By Custom Field Value and Edit default Query

--- functions.php
+++ functions.php
@@ -5,7 +5,6 @@ function university_files() {
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
-
     wp_enqueue_style('university-main-styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university-extra-styles', get_theme_file_uri('/build/index.css'));
 }
@@ -17,3 +16,22 @@ function university_features() {
 }
 
 add_action('after_setup_theme', 'university_features');
+
+function university_adjust_queries($query) { 
+    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+                'key' => 'event_date',
+                'compare' => '<=',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        ));
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
